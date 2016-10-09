4c04f65fb9e62b49079f765b0898125024ba5876
dafma <mar_tp@hotmail.es>
2016-10-09 14:32:22 -0500

    add todo

---
+++ README.md
@@ -0,0 +1,1 @@
+"single page for events write in django" 
---
+++ subscripciones/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render
+from django.conf import settings
+from django.core import mail
+from django.http import HttpResponseRedirect, Http404
+from django.shortcuts import render
+from django.template.loader import render_to_string
+from subscripciones.forms import SubscriptionForm
+from subscripciones.models import Subscription
+
+
+def subscribe(request):
+    if request.method == 'POST':
+        return create(request)
+    else:
+        return new(request)
+
+
+def create(request):
+    form = SubscriptionForm(request.POST)
+
+    if not form.is_valid():
+        return render(request, 'subscription_form.html', {'form': form})
+
+    subscription = Subscription.objects.create(**form.cleaned_data)
+
+    _send_mail('Confirmação de inscrição',
+               settings.DEFAULT_FROM_EMAIL,
+               subscription.email,
+               'subscription_email.txt',
+               {'subscription': subscription})
+
+    return HttpResponseRedirect('/inscricao/{}/'.format(subscription.pk))
+
+
+def new(request):
+    return render(request, 'subscription_form.html', {'form': SubscriptionForm()})
+
+
+def detail(request, pk):
+    try:
+        subscription = Subscription.objects.get(pk=pk)
+    except Subscription.DoesNotExist:
+        raise Http404
+
+    return render(request, 'subscription_detail.html', {'subscription': subscription})
+
+
+def _send_mail(subject, from_, to, template_name, context):
+    body = render_to_string(template_name, context)
+    mail.send_mail(subject, body, from_, [from_, to])
+# Create your views here.
